bd2cdd699d93e990e6d8c4a20304a7048bfa9dbb
Jaimin Patel <1994.jaimin@gmail.com>
2025-11-26 00:32:43 +0530

    FIxed cors error

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ const cors = require('cors');
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
